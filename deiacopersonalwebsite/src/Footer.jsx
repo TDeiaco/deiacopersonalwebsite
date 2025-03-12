cb0613efc926c979c056f854221015b6cbfe5593
taylor <tdeiaco@gmail.com>
2025-03-11 19:44:27 -0700

    first commit

---
+++ deiacopersonalwebsite/src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer()
+{
+    return (
+        <footer>
+           <p>&Copy; {new Date().getFullYear()} Taylor Deiaco Developments</p>>
+        </footer>
+    );
+
+}
+
+export default Footer
---
+++ deiacopersonalwebsite/src/Header.jsx
@@ -0,0 +1,20 @@
+
+function Header()
+{
+    return (
+        <header>
+            <h1>Taylor Deiaco</h1>
+            <nav>
+                <ul>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Backgrounds</a></li>
+                    <li><a href='#'>Software</a></li>
+                    <li><a href='#'>About</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+
+}
+
+export default Header
