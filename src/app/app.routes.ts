4151dc08c2efd581927b5035c6fe51855d21acb5
taylor <tdeiaco@gmail.com>
2025-03-15 11:22:13 -0700

    adds career component, adds home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { PhotographyComponent } from './photography/photography.component';
 import { SoftwareComponent } from './software/software.component';
+import { CareerComponent } from './career/career.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'photography', component: PhotographyComponent },
     { path: 'software', component: SoftwareComponent },
+    { path: 'career', component: CareerComponent },
   ];
