ff68e37070e08046aa04635321a2420606313401
taylor <tdeiaco@gmail.com>
2025-03-15 11:05:33 -0700

    adds pages and proper routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PhotographyComponent } from './photography/photography.component';
+import { SoftwareComponent } from './software/software.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'photography', component: PhotographyComponent },
+    { path: 'software', component: SoftwareComponent },
+  ];
